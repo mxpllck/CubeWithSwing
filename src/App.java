e15caf2cfaf5a82aac7fd9a5c8b3a08688d23dfd
Maxwell Pollack <maxwell_pollack@zoho.com>
2019-05-13 11:18:13 -0700

    added key press event manager

--- src/App.java
+++ src/App.java
@@ -1,31 +1,65 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 public class App {
+	//x y z, z is up
+	private int[] playerPos = {
+			0, 0, 0
+	};
+	//3 numbers define a point on the sphere given by
+	//1=x^2+y^2+z^2
+	private int[] playerDirection = {
+			1, 0, 0
+	};
     public static void main(String[] args)
     {
-        JFrame frame = new JFrame("Squares");
-        frame.setDefaultLookAndFeelDecorated(true);
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		JFrame frame = new JFrame("Cube");
         frame.setSize(400, 400);
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		frame.addKeyListener(new KeyListener() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				if ((e.getKeyChar() == 'w')) {
+					System.out.println("w key pressed.");
+				} else if (e.getKeyChar() == 'a') {
 
-        Poly square = new Poly();
+				} else if (e.getKeyChar() == 's') {
+
+				} else if (e.getKeyChar() == 'd') {
+
+				}
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+
+			}
+		});
+		int[] xCoord = {0, 0, 50, 50};
+		int[] yCoord = {0, 50, 50, 0};
+		Poly square = new Poly(xCoord, yCoord);
         frame.add(square);
         frame.setVisible(true);
     }
 }
 
 class Poly extends JPanel {
-    private int[] x = {
-            0, 0, 50, 50
-    };
-    private int[] y = {
-            0, 50, 50, 0
-    };
-    Poly(){
+	private int[] x;
+	private int[] y;
 
+	Poly(int[] xCoord, int[] yCoord) {
+		x = xCoord;
+		y = yCoord;
     }
     public void paint(Graphics g){
         g.drawPolygon(x, y, 4);
