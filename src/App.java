301084ded68b1d36c69ba2a49c64c8caa9ba3cc9
Maxwell Pollack <maxwell_pollack@zoho.com>
2019-05-23 12:07:52 -0700

    added Player class, removed duplicate code

--- src/App.java
+++ src/App.java
@@ -1,71 +1,3 @@
-//import javax.swing.*;
-//import java.awt.*;
-//import java.awt.event.KeyEvent;
-//import java.awt.event.KeyListener;
-//
-//import static java.awt.event.KeyEvent.VK_UP;
-//import static java.awt.event.KeyEvent.VK_W;
-//import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
-//
-//public class App {
-//	//x y z, z is up
-//	private int[] playerPos = {
-//			0, 0, 0
-//	};
-//	//3 numbers define a point on the sphere given by
-//	//1=x^2+y^2+z^2
-//	private int[] playerDirection = {
-//			1, 0, 0
-//	};
-//	public static void main(String[] args)
-//    {
-//		JFrame.setDefaultLookAndFeelDecorated(true);
-//		JFrame frame = new JFrame("Cube");
-//        frame.setSize(400, 400);
-//        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-//		frame.setVisible(true);
-//		//just example values.  Will contain data later.
-//		int[] xCoords = {0, 0, 50, 50};
-//		int[] yCoords = {0, 50, 50, 0};
-//		Poly square = new Poly(xCoords, yCoords);
-//
-//		frame.addKeyListener(new KeyListener() {
-//			@Override
-//			public void keyTyped(KeyEvent e) {
-//
-//			}
-//
-//			@Override
-//			public void keyPressed(KeyEvent e) {
-//
-//			}
-//
-//			@Override
-//			public void keyReleased(KeyEvent e) {
-//				if ((e.getKeyCode() == VK_W)) {
-//					System.out.println("w key released.");
-//					frame.add(square);
-//					frame.revalidate();
-//					frame.repaint();
-//				}
-//				//else if (e.getKeyChar() == 'a') {
-////
-////				} else if (e.getKeyChar() == 's')
-////
-////				} else if (e.getKeyChar() == 'd') {
-////
-////				}
-//				else if (e.getKeyCode() == VK_UP) {
-//					System.out.println("up arrow released");
-//					//frame.remove(square);
-//					frame.repaint();
-//				}
-//			}
-//		});
-//    }
-//}
-//
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
@@ -130,21 +62,25 @@ public class App extends JFrame {
 
 //holds cube coordinates, the update function changes displayX and displayY in keeping with the player angle and movement
 class Cube extends JPanel {
-	//hidden coords for the 3d cube
-	private int[] cubeX = {0, 100, 100, 0,};
-	private int[] cubeY = {0, 0, 100, 100,};
-	private int[] cubeZ = {0, 0, 0, 0, 100, 100, 100, 100};
+	//hidden coords for the 3d cube, which is composed of two squares, one above the other
+	//first square: (0, 0, 0), (0, 100, 0), (100, 100, 0), (100, 0, 0)
+	//second square
 
 	//available for printing onto a 2d screen; this is the projection
-	private int[] displayX = {0, 100, 100, 0,};
-	private int[] displayY = {0, 0, 100, 100,};
+	private int[] displayX1 = {100, 0, 100, 0};
+	private int[] displayY1 = {0, 100, 100, 0};
 
+
+	//adjusts display coordinates
 	void update(Player player) {
+		//IF entire cube is within acceptable range, project the 3d cube onto the 400x400 window drawable area.
+		//ELSE player cannot see the cube
+		//player is facing towards
 
 	}
 
 	public void paint(Graphics g) {
-		g.drawPolygon(displayX, displayY, 4);
+		g.drawPolygon(displayX1, displayY1, 4);
 	}
 }
 
