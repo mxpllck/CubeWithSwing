f85553f6565af0a0f1aa3ec2284237c581a9cb2a
Maxwell Pollack <maxwell_pollack@zoho.com>
2019-05-28 11:13:13 -0700

    creating structrure

--- src/App.java
+++ src/App.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.geom.Ellipse2D;
+
 import static java.awt.event.KeyEvent.*;
 
 public class App extends JFrame {
@@ -14,7 +16,8 @@ public class App extends JFrame {
 		setSize(400, 400);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
-
+		Cube cube = new Cube();
+		add(cube);
 		addKeyListener(new KeyListener() {
 			//these two are empty and do not trigger anything.
 			@Override
@@ -31,15 +34,13 @@ public class App extends JFrame {
 				releasedKey(e);
 			}
 		});
-		Cube cube = new Cube();
-		add(cube);
-
 	}
 
 	//used for overloading KeyListener.keyReleased() for a specific instance in the App constructor.
 	private void releasedKey(KeyEvent e) {
 		if ((e.getKeyCode() == VK_W)) {
 			System.out.println("w key released.");
+
 		} else if (e.getKeyCode() == VK_S) {
 			System.out.println("s key released.");
 		} else if (e.getKeyCode() == VK_A) {
@@ -55,6 +56,7 @@ public class App extends JFrame {
 		} else if (e.getKeyCode() == VK_RIGHT) {
 			System.out.println("right arrow released");
 		}
+
 		revalidate();
 		repaint();
 	}
@@ -64,27 +66,45 @@ public class App extends JFrame {
 class Cube extends JPanel {
 	//hidden coords for the 3d cube, which is composed of two squares, one above the other
 	//first square: (0, 0, 0), (0, 100, 0), (100, 100, 0), (100, 0, 0)
-	//second square
+	//second square: (0, 0, 100), (0, 100, 0), (100, 100, 0), (100, 0, 0)
+
+	private final int[] objectiveXCoords = {0, 0, 100, 100, 0, 0, 100, 100};
+	private final int[] objectiveYCoords = {0, 100, 100, 0, 0, 100, 100, 0};
+	private final int[] objectiveZCoords = {0, 0, 0, 0, 100, 100, 100, 100};
+
+
+	private int[] relativeXCoords = objectiveXCoords;
+	private int[] relativeYCoords = objectiveYCoords;
+	private int[] relativeZCoords = objectiveZCoords;
 
 	//available for printing onto a 2d screen; this is the projection
-	private int[] displayX1 = {100, 0, 100, 0};
-	private int[] displayY1 = {0, 100, 100, 0};
+	private int[] displayX = relativeXCoords;
+	private int[] displayZ = relativeYCoords;
 
 
 	//adjusts display coordinates
 	void update(Player player) {
-		//IF entire cube is within acceptable range, project the 3d cube onto the 400x400 window drawable area.
-		//ELSE player cannot see the cube
-		//player is facing towards
+		for (int i = 0; i < 8; ++i)
+			relativeXCoords[0] = objectiveXCoords[0] - player.playerXYZ[0];
+		//for each point within acceptable range:
+		//it updates the point positions, or else puts offscreen
 
 	}
 
 	public void paint(Graphics g) {
-		g.drawPolygon(displayX1, displayY1, 4);
+		super.paintComponent(g);
+		Graphics2D g2d = (Graphics2D) g;
+		// Assume x, y, and diameter are instance variables.
+		Ellipse2D.Double[] circles;
+		//for(int i = 0; i < 8; i++)
+		//Ellipse2D.Double circle = new Ellipse2D.Double(100, 200, 10, 10);
+		//g2d.fill(circle);
+		Ellipse2D.Double circle = new Ellipse2D.Double(100, 200, 10, 10);
+		g2d.fill(circle);
 	}
 }
 
 class Player {
-	public int[] playerXYZ = {0, 0, 0};
-	public int[] playerDirXYZ = {0, 1, 0};
+	int[] playerXYZ = {0, 0, 0};
+	int[] playerDirXYZ = {0, 1, 0};
 }
